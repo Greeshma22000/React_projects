b289af641e1516685ac1579a823dede247f9f6c5
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-11-20 10:48:06 +0530

    .

--- Task-Planner/src/App.jsx
+++ Task-Planner/src/App.jsx
@@ -4,7 +4,17 @@ import "animate.css";
 const App = () => {
   return (
     <div className='bg-gray-200 h-screen overflow-hidden'>
-      <nav className='bg-white h-[60px] fixed top-0 left-0 w-full'></nav>
+      <nav className='bg-white h-[60px] fixed top-0 left-0 w-full'>
+
+      </nav>
+      
+      <section className='bg-violet-300 fixed top-[60px] left-0 h-[calc(100%-120px)] w-full'>
+        
+      </section>
+
+      <footer className='bg-white h-[60px] fixed bottom-0 left-0 w-full'>
+
+      </footer>
     </div>
   )
 }
