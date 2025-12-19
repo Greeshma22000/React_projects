98d2b5c41bc7416a06b07c48a625f394346ced24
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-19 15:07:39 +0530

    .

--- Task-Planner/src/store/usePlanner.js
+++ Task-Planner/src/store/usePlanner.js
@@ -1,8 +1,26 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { persist} from 'zustand/middleware'
 
-export const usePlanner = create((set)=>({
-    tasks: [],
-    addTask: (payload)=>set((state)=>({
-        tasks: [...state.tasks, payload]
-    }))
-}))
+export const usePlanner = create(persist(
+    (set)=>({
+        tasks: [],
+        addTask: (payload)=>set((state)=>({
+            tasks: [...state.tasks, payload]
+        })),
+        deleteTask: (id)=>set((state)=>({
+            tasks: state.tasks.filter((task)=>task.id !== id)
+        })),
+        updateStatus: (id, status)=>set((state)=>({
+            tasks: state.tasks.filter((task)=> {
+                if(task.id === id)
+                    task.status = status
+
+                return task
+            })
+        })),
+        deleteAllTask: ()=>set(()=>({
+            tasks: []
+        }))
+    }),
+    {name: "planner"}
+))
